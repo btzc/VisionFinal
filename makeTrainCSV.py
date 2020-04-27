fc82eeb6062263e49063f0c20592c537974f83d6
rowanbk <bk@rowan.engineer>
2020-04-26 23:34:21 -0700

    Here's da bish

---
+++ makeTrainCSV.py
@@ -0,0 +1,16 @@
+from glob import glob
+
+people = "./lfw/*"
+neg_person = glob(people)[-1]
+with open('./train1.csv', 'w') as t:
+    t.write('id,anchor_img,pos_img\n')
+    c = 1
+    for person in glob(people):
+        neg_name = neg_person.rsplit('/',1)[-1]
+        neg_img = neg_person + '/' + neg_name +'_0001.jpg'
+        name = person.rsplit('/',1)[-1]
+        anchor_img = person + '/' + name +'_0001.jpg'
+        for pos_img in glob(person+'/*'):
+            t.write(str(c)+','+anchor_img+','+pos_img+','+neg_img+'\n')
+        neg_person = person
+        c += 1
