0682f746c3859332b53c79ea68a6dc2a70d56cf3
rowanbk <bk@rowan.engineer>
2020-04-29 19:03:58 -0700

    I'm using my one-time for this to work

--- newEvalSiamese.py
+++ newEvalSiamese.py
@@ -11,6 +11,26 @@ import numpy as np
 from dataloader import load_train_test
 from evaluation import *
 
+def triplet_loss(inputs, dist='euclidean', margin='maxplus'):
+    anchor, positive, negative = inputs
+    positive_distance = K.square(anchor - positive)
+    negative_distance = K.square(anchor - negative)
+    if dist == 'euclidean':
+        positive_distance = K.sqrt(K.sum(positive_distance, axis=-1, keepdims=True))
+        negative_distance = K.sqrt(K.sum(negative_distance, axis=-1, keepdims=True))
+    elif dist == 'sqeuclidean':
+        positive_distance = K.sum(positive_distance, axis=-1, keepdims=True)
+        negative_distance = K.sum(negative_distance, axis=-1, keepdims=True)
+    loss = positive_distance - negative_distance
+    if margin == 'maxplus':
+        loss = K.maximum(0.0, 2 + loss)
+    elif margin == 'softplus':
+        loss = K.log(1 + K.exp(loss))
+
+    returned_loss = K.mean(loss)
+    return returned_loss
+
+
 json_file = open('./saved_model/encoding_network_arch.json', 'r')
 loaded_model_json = json_file.read()
 json_file.close()
@@ -42,6 +62,7 @@ next_element = iterator.get_next()
 # Use the agnostic tensorflow session from Keras
 sess = K.get_session()
 sess.run(iterator.initializer, feed_dict={filenames: TEST_INPUT_PATHS})
+losses = []
 while True:
     try:
         anchor_path, pos_path, neg_path = sess.run(next_element)
@@ -55,23 +76,34 @@ while True:
             anchor_img = image.img_to_array(anchor_img)
             anchor_img = np.expand_dims(anchor_img, axis=0)
             anchor_img = preprocess_input(anchor_img)
-            #anchor_imgs = np.append(anchor_imgs, anchor_img, axis=0)
+            anc_encoding_net_test_inputs = np.append(encoding_net_test_inputs,
+                                                anc_img, axis=0)
+            anc_encoding = encoding_network.predict([anc_encoding_net_test_inputs],
+                                                batch_size = 1,verbose = 1)
 
             pos_img = image.load_img(pos_path[j], target_size=(224, 224))
             pos_img = image.img_to_array(pos_img)
             pos_img = np.expand_dims(pos_img, axis=0)
             pos_img = preprocess_input(pos_img)
-            encoding_net_test_inputs = np.append(encoding_net_test_inputs,
+            pos_encoding_net_test_inputs = np.append(encoding_net_test_inputs,
                                                 pos_img, axis=0)
-            test_encoding = encoding_network.predict([encoding_net_test_inputs],
+            pos_encoding = encoding_network.predict([pos_encoding_net_test_inputs],
                                                 batch_size = 1,verbose = 1)
 
             neg_img = image.load_img(neg_path[j], target_size=(224, 224))
             neg_img = image.img_to_array(neg_img)
             neg_img = np.expand_dims(neg_img, axis=0)
             neg_img = preprocess_input(neg_img)
-            #neg_imgs = np.append(neg_imgs, neg_img, axis=0)
+            neg_encoding_net_test_inputs = np.append(encoding_net_test_inputs,
+                                                neg_img, axis=0)
+            neg_encoding = encoding_network.predict([neg_encoding_net_test_inputs],
+                                                batch_size = 1,verbose = 1)
+            loss = Lambda(triplet_loss)([encoding_anchor, encoding_pos, encoding_neg])
+            print("This is loss: ", loss)
+            losses.append(loss)
 
     except tf.errors.OutOfRangeError:
         print("Out of range error triggered (looped through training set).")
         break
+
+print(np.mean(losses))
